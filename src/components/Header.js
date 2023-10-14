d3c31d5a7fc5cb76caf9d38f41f2179b33f75172
govindulasaisanthosh <govindulasai.santhosh2021@vitstudent.ac.in>
2023-10-14 21:29:22 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>saisanthosh</h1>
+      <p>Web Developer</p>
+    </header>
+  );
+}
+
+export default Header;
+
---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      Fullstack
+      python
+    </section>
+  );
+}
+
+export default Projects;
